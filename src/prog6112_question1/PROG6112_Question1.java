6ecafe95df5297e4f83dab8869870d97a2448381
ST10450853-NKOSINATHIMAQUNGELA <ST10450853@vcconnect.edu.za>
2025-11-10 10:19:40 +0200

    initial commit

---
+++ src/prog6112_question1/PROG6112_Question1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prog6112_question1;
+
+/**
+ *
+ * @author maqun
+ */
+public class PROG6112_Question1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
